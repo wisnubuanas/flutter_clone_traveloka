4c39954c0d4733e495182552541b8eba2239fdd5
wisnubuanas <king.wisnu50@gmail.com>
2023-01-08 23:46:46 +0700

    search

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -10,8 +10,31 @@ class Profil extends StatefulWidget {
 class _ProfilState extends State<Profil> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profil"),
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide.none),
+                  filled: true,
+                  fillColor: Colors.white),
+              // style: Colors.white,
+            ),
+          ),
+        ),
+        title: Text("Traveloka"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_horiz),
+          ),
+        ],
+      ),
     );
   }
 }
